d91d4d92ec54c1fb90249c4b3e05373ff18513a2
Mikael <Mikael.behrami@gmail.com>
2021-09-28 11:03:27 +0200

    wspgitk

--- webbserv1-main/db_connection.php
+++ webbserv1-main/db_connection.php
@@ -5,7 +5,7 @@ function OpenCon()
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
- $db = "mikeärgay";
+ $db = "test";
 
 
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
